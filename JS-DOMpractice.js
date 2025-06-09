4a8c3699cec247e53a24b54a88e8f656bcd6a0ff
Andrea <andreapalacios481@gmail.com>
2025-06-09 16:49:59 -0600

    refactor! example

--- JS-DOMpractice.js
+++ JS-DOMpractice.js
@@ -88,36 +88,4 @@ function addSong(artistValue, titleValue) {
   const songTemplate = document.querySelector("#song-template").content;
 }*/
 
-const songsContainer = document.querySelector(".songs-container");
-const addButton = document.querySelector(".input__btn_action_add");
-const form = document.forms.add;
-const artist = form.elements.artist;
-const title = form.elements.title;
 
-function addSong(artistValue, titleValue) {
-  const songTemplate = document.querySelector("#song-template").content;
-  const songElement = songTemplate.cloneNode(true);
-
-  songElement.querySelector(".song__artist").textContent = artistValue;
-  songElement.querySelector(".song__title").textContent = titleValue;
-
-  songsContainer.append(songElement);
-}
-
-function setSubmitButtonState(isFormValid) {
-  if (isFormValid) {
-    addButton.removeAttribute("disabled");
-    addButton.classList.remove("input__btn_disabled");
-  } else {
-    addButton.setAttribute("disabled", true);
-    addButton.classList.add("input__btn_disabled");
-  }
-}
-
-form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  addSong(artist.value, title.value);
-  artist.value = "";
-  title.value = "";
-  setSubmitButtonState(false);
-});
