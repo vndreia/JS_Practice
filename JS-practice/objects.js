9305d0def1aba3a77ba0da0877cb392b2507c6c1
Andrea <andreapalacios481@gmail.com>
2025-06-20 14:20:54 -0600

    add: private properties to the Song OBJECT

--- JS-practice/objects.js
+++ JS-practice/objects.js
@@ -443,3 +443,45 @@ class PodcastEpisode {
     return `${minutes}:${seconds > 9 ? seconds : "0" + seconds};` //Ternary operator: condition ? expiftrue : expiffalse
   } 
 }
+//Now we indicate with "_" which parts of the code are private and aren't supposed to be manipulated
+
+class Song {
+  constructor(title, artist, releaseYear) {
+    this._title = title;
+    this._artist = artist;
+    this._releaseYear = releaseYear;
+    this.isLiked = false;
+  }
+
+  like() {
+    this.isLiked = !this.isLiked;
+  }
+
+  getSongInfo() {
+    return `${this._artist} - ${this._title} (${this._releaseYear})`
+  }
+}
+
+class PodcastEpisode {
+  constructor(title, artist, guest, duration) {
+    this._title = title;
+    this._artist = artist;
+    this._guest = guest;
+    this._duration = duration;
+    this.isLiked = false;
+  }
+
+  like() {
+    this.isLiked = !this.isLiked;
+  }
+
+  getEpisodeInfo() {
+    return `${this._artist}. "${this._title}" - ${this._guest} (${this.getFormattedDuration()})`;
+  }
+
+  getFormattedDuration() {
+    const minutes = Math.floor(this._duration / 60); // el número total de minutos
+    const seconds = this._duration % 60; // el resto de la división por 60
+    return `${minutes}:${seconds > 9 ?  seconds : 0 + seconds}`;
+  }
+}
