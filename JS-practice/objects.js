b49cb5cd1589df3fd4d7107b72fc0d6f7197ac07
Andrea <andreapalacios481@gmail.com>
2025-06-28 16:32:06 -0600

    add: objects changes

--- JS-practice/objects.js
+++ JS-practice/objects.js
@@ -652,3 +652,153 @@ añadir una nueva clase solo requerirá
   // ...
   return this._element;
 }
+
+//You can get everything from dot notation with objects, even nested properties:
+const persona = {
+  nombre: 'Andrea',
+  direccion: {
+    calle: {
+      nombre: 'Av. Reforma',
+      numero: 123
+    }
+  }
+};
+
+// Acceder al número
+console.log(persona.direccion.calle.numero); // 123
+
+
+//An E-commerce example that creates a product object:
+const items = [
+  {
+    image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/oop/moved_card_detail.jpg",
+    title: "BIOLOID",
+    description: "Robot Kit es una popular serie de kits de construcción de robots de la empresa Robotis.La serie incluye varios kits diseñados tanto para principiantes como para usuarios avanzados.En este kit, encontrarás todo lo que necesitas para construir robots únicos y hacer diseños personalizados.El set incluye un software de programación, un mando a distancia, servomotores y mucho más.",
+    price: "$999"
+  },
+  {
+    image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/oop/moved_card_detail.jpg",
+    title: "BIOLOID Premium kit",
+    description: "BIOLOID Premium Kit tiene todo lo necesario para construir robots bípedos e incluye el servomotor Dynamixel Smart y el controlador CM-530.BIOLOID Premium Kit es perfecto para la educación, el entretenimiento y las competiciones de robótica",
+    price: "$1800"
+  },
+  {
+    image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/oop/moved_card_detail2.png",
+    title: "Airwheel",
+   description: "Este modelo de bicicleta permite utilizar tanto el pedal como el motor eléctrico, así como la combinación de ambos. La Airwheel R8 está equipada con un set de baterías desmontables, que garantizan un suministro de energía suficiente y hacen que tus viajes sean más largos.",
+    price: "$2000"
+  },
+];
+
+class Card {
+  constructor(title, description, price, image) {
+    this._title = title;
+    this._description = description;
+    this._price = price;
+    this._image = image;
+  }
+
+  _getTemplate() {
+    const cardElement = document
+      .querySelector('.horizontal-card')
+      .content
+      .querySelector('.card')
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+
+    this._element.querySelector(".card__image").style.backgroundImage = `url(${this._image})`;
+    this._element.querySelector(".card__title").textContent = this._title;
+    this._element.querySelector(".card__info").textContent = this._description;
+    this._element.querySelector(".card__price-property").textContent = this._price;
+
+    return this._element;
+  }
+}
+
+items.forEach((item) => {
+  const card = new Card(item.title, item.description, item.price, item.image);
+  const cardElement = card.generateCard();
+
+  // Añadir al DOM
+  document.querySelector(".card-list__items").append(cardElement); //Selecciona el div vacío 
+  //El contenedor vacío donde irán las cards con la clase card-list__items
+});
+
+//Now the e-commerce example is optimized to make it more reusable, but that makes it MORE ABSTRACT:
+const items = [
+  {
+    image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/oop/moved_card_detail.jpg",
+    title: "BIOLOID",
+    description: "Robot Kit es una popular serie de kits de construcción de robots de la empresa Robotis.La serie incluye varios kits diseñados tanto para principiantes como para usuarios avanzados.En este kit, encontrarás todo lo que necesitas para construir robots únicos y hacer diseños personalizados.El set incluye un software de programación, un mando a distancia, servomotores y mucho más.",
+    price: "$999"
+  },
+  {
+    image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/oop/moved_card_detail.jpg",
+    title: "BIOLOID Premium kit",
+    description: "BIOLOID Premium Kit tiene todo lo necesario para construir robots bípedos e incluye el servomotor Dynamixel Smart y el controlador CM-530.BIOLOID Premium Kit es perfecto para la educación, el entretenimiento y las competiciones de robótica",
+    price: "$1800"
+  },
+  {
+    image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/oop/moved_card_detail2.png",
+    title: "Airwheel",
+   description: "Este modelo de bicicleta permite utilizar tanto el pedal como el motor eléctrico, así como la combinación de ambos. La Airwheel R8 está equipada con un set de baterías desmontables, que garantizan un suministro de energía suficiente y hacen que tus viajes sean más largos.",
+    price: "$2000"
+  },
+];
+
+class Card {
+  constructor(data, cardSelector) { //The parameter data is an object with the properties title, description, price, and image
+    this._title = data.title;  
+    this._description = data.description;
+    this._price = data.price;
+    this._image = data.image;
+    this._cardSelector = cardSelector; /*  ¿Pero qué es this._cardSelector?
+Eso lo definiste en el constructor con:
+this._cardSelector = cardSelector;
+Y eso significa que ahora puedes pasarle diferentes strings como:
+
+
+'.horizontal-card'
+'.vertical-card'*/
+  }
+
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._cardSelector)  //This changed
+      .content
+      .querySelector('.card')
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+
+    this._element.querySelector(".card__image").style.backgroundImage = `url(${this._image})`;
+    this._element.querySelector(".card__title").textContent = this._title;
+    this._element.querySelector(".card__info").textContent = this._description;
+    this._element.querySelector(".card__price-property").textContent = this._price;
+
+    return this._element;
+  }
+}
+
+items.forEach((item) => {
+ const card = new Card(item, '.horizontal-card');
+  const cardElement = card.generateCard();
+
+  // Añadir al DOM
+  document.querySelector(".card-list__items").append(cardElement); //Selecciona el div vacío 
+  //El contenedor vacío donde irán las cards con la clase card-list__items
+});
+
+//Básicamente siento que es mas escalable y reutilizable, 
+// pero mas abstracto porque no estoy usando un valor que le corresponda a ningun item, sino a lo que represente el item "de ese momento"
+//data (el objeto item) → representa el contenido de una card.
+//cardSelector → representa la estructura visual donde ese contenido se va a mostrar.
