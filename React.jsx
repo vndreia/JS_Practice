f719bfb7b3fbbdbee3130f05015ae7183c34de99
Andrea <andreapalacios481@gmail.com>
2025-12-02 00:23:52 -0600

    add: GONG!

--- React.jsx
+++ React.jsx
@@ -439,3 +439,107 @@ input.value = '';
 	ReactDOM.render((
 	  <GoodDeeds />
 	), document.querySelector('#root'));
+
+
+  //An example of hooks with a cursor effect:
+
+  function NeonCursor() {
+  const [position, setPosition] = React.useState({ top: 0, left: 0 });
+
+  React.useEffect(() => {
+    function handleMouseMove(event) {
+      setPosition({
+        top: event.pageY,
+        left: event.pageX,
+      });
+    }
+
+   // lista de acciones dentro de un hook
+    document.addEventListener('mousemove', handleMouseMove);
+    document.body.classList.add('no-cursor');
+
+    // estamos devolviendo una función que elimina nuestros efectos
+    return () => {
+      document.body.classList.remove('no-cursor');
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  });
+
+  return (
+    <img
+      src="./cursor.png"
+      width={30}
+      style={{
+        position: 'absolute',
+        top: position.top,
+        left: position.left,
+        pointerEvents: 'none',
+      }}
+    />
+  );
+}
+
+
+{/*A beautiful example where I use the audio object 4 the first time  */}
+function playSound(fileName) {
+  const audio = new Audio();
+  audio.src = `https://code.s3.yandex.net/web-code/react/${fileName}`;
+  audio.play();
+}
+
+function App() {
+  const [isTimeShown, setIsTimeShown] = React.useState();
+
+  function handleChange() {
+    setIsTimeShown(!isTimeShown);
+  }
+
+  return (
+    <>
+      <label>
+        <input type="checkbox" onChange={handleChange}/>
+        — Habilitar tiempo
+      </label>
+      {isTimeShown && <Time/>}
+    </>
+  );
+}
+
+function Time() {
+  const [hours, setHours] = React.useState(0);
+  const [minutes, setMinutes] = React.useState(0);
+  const [seconds, setSeconds] = React.useState(0);
+  const [milliseconds, setMilliseconds] = React.useState(0);
+
+  React.useEffect(() => {
+    requestAnimationFrame(() => {
+      const date = new Date();
+      setMilliseconds(date.getMilliseconds());
+      setSeconds(date.getSeconds());
+      setMinutes(date.getMinutes());
+      setHours(date.getHours());
+    });
+  });
+
+  React.useEffect(() => {
+    playSound('tick.mp3');
+  }, [seconds]);
+
+  React.useEffect(() => {
+    playSound('gong.mp3');
+
+    return () => {
+      playSound('gong.mp3');
+    };
+  }, []);
+
+  
+
+  return (
+    <h2>{hours}:{minutes}:{seconds}:{milliseconds}</h2>
+  );
+}
+
+ReactDOM.render((
+  <App/>
+), document.querySelector('#root'));
